7b858fb160585df45e40776e3ca3f48cd7ddd86f
TJ Nickerson <TJPNick@gmail.com>
2023-05-02 12:35:11 -0400

    added army images

--- initial.sql
+++ initial.sql
@@ -20,7 +20,7 @@ CREATE TABLE deck(
 );
 INSERT INTO deck (deck_name, deck_icon)
 VALUES
-("Army Deck", "ham.jpg");
+("Army Deck", "tank.png");
 CREATE TABLE deck_card(
     deck_id int,
     card_id int, 
@@ -32,16 +32,16 @@ CREATE TABLE deck_card(
 );
 INSERT INTO deck_card (deck_id, card_id, health, damage, card_sprite, card_name)
 VALUES 
-(1,1,0,2,"ham.jpg", "Recruit"),
-(1,2,0,2,"ham.jpg", "Recruit"),
-(1,3,0,10,"ham.jpg", "Tank"),
-(1,4,3,0,"ham.jpg", "Medic"),
-(1,5,3,0,"ham.jpg", "Medic"),
-(1,6,4,1,"ham.jpg", "Officer"),
-(1,7,0,5,"ham.jpg", "Soldier"),
-(1,8,0,5,"ham.jpg", "Soldier"),
-(1,9,0,5,"ham.jpg", "Soldier"),
-(1,10,0,0,"ham.jpg", "Mobilization");
+(1,1,0,2,"recruit.png", "Recruit"),
+(1,2,0,2,"recruit.png", "Recruit"),
+(1,3,0,10,"tank.png", "Tank"),
+(1,4,3,0,"medic.png", "Medic"),
+(1,5,3,0,"medic.png", "Medic"),
+(1,6,4,1,"officer.png", "Officer"),
+(1,7,0,5,"soldier.png", "Soldier"),
+(1,8,0,5,"soldier.png", "Soldier"),
+(1,9,0,5,"soldier.png", "Soldier"),
+(1,10,0,0,"mobilization.png", "Mobilization");
 CREATE TRIGGER `giveDefaultCard` AFTER INSERT ON `user` FOR EACH ROW INSERT INTO cardclash.deck_ownership( user_id, deck_id ) VALUES (new.user_id, 1);
 CREATE TABLE game_player(
     user_id int,
@@ -50,5 +50,6 @@ CREATE TABLE game_player(
 );
 CREATE TABLE game_card(
     user_id int,
-    card_id int
+    card_id int, 
+    played TINYINT(1)
 );
